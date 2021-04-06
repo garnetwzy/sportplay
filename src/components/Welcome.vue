cdb5b4b9d7102614f6429812cd15910f72f2f311
JingZheng <song.jingz@husky.neu.edu>
2021-04-06 16:28:01 +0800

    create placeholder components

---
+++ src/components/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="welcome-container">
+        <div class="image">
+            <img src="https://www.supplementvibes.com/wp-content/uploads/2021/03/Fitness.jpg" alt="image" width="100%" height="100%">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+<style lang="less" scoped>
+
+</style>
