fe899321db94cb86062d154fd94e70c05507cc1b
garnetwzy <596913416@qq.com>
2021-04-26 21:55:13 -0700

    fix

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+const cors = require('cors');
 const serveStatic = require('serve-static')
 const path = require('path')
 
 var app = express();
+app.use(cors())
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 8080
