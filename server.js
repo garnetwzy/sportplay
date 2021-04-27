ab58de682519f3dcfe11d01894f946145e9f4922
garnetwzy <596913416@qq.com>
2021-04-26 22:02:08 -0700

    fix

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const path = require('path')
 
 var app = express();
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 8080
